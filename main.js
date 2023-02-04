c50cebb5e66569128b24e7a00500f2cf61ab4234
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-02-04 17:06:17 -0300

    Push personas

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-class Consultorio {
-    constructor(consult, nombre) {
-        super(nombre);
-        this.nombre = nombre;
-        this.consult = consult;
-    }
-}
+let catalogo =
+    [{ _nombre: 'Pamela', _edad: '21', _rut: '2344234', _diagn: 'Fiebre uterina' },
+    { _nombre: 'Peluca', _edad: '33', _rut: '4234', _diagn: 'Gripe' },
+    { _nombre: 'Pedro', _edad: '55', _rut: '52352', _diagn: 'Infeccion urinaria' },
+    { _nombre: 'Paola', _edad: '34', _rut: '52352', _diagn: 'Estress' },
+    { _nombre: 'Piero', _edad: '76', _rut: '7564', _diagn: 'Psicosis' }]
 
+class Paciente {
 
-class Paciente extends Consultorio{
     constructor(nombre, edad, rut, diagn) {
         this.nombre = nombre;
         this.edad = edad;
         this.rut = rut;
         this.diagn = diagn;
     }
+
     get nombre() {
         return this._nombre;
     }
     set nombre(nombre) {
         this._nombre = nombre;
     }
+
     get edad() {
         return this._edad;
     }
@@ -40,27 +41,22 @@ class Paciente extends Consultorio{
     set diagn(diagn) {
         this._diagn = diagn;
     }
-
-    
-
+    agregarPaciente() {
+        catalogo.push({ _nombre: this._nombre, _edad: this._edad, _rut: this._rut, _diagn: this._diagn });
+    console.log(this._nombre)
+    }
 }
 
 
 
+var persona1 = new Paciente('Carlos', '33', '17163102-5', 'Gripe');
+var persona2 = new Paciente('Josefa', '25', '20122345-3', 'Sarna');
+var persona3 = new Paciente('Juan', '28', '19456345-3', 'Malaria');
+var persona4 = new Paciente('Daniela', '50', '8928876-2', 'Esguince');
+var persona5 = new Paciente('Ignacio', '40', '10456345-5', 'Tetanos');
 
-
-
-// var obj = {};
-
-// return obj;
-// }
-// function CreateNewConsult(nombreConsult){
-//     var obj2 = {};
-//     obj2.nombreConsult = nombreConsult
-// }
-
-// var persona1 = CreateNewPatient('Carlos','33', '17163102-5','Gripe');
-// var persona2 = CreateNewPatient('Josefa','25', '20122345-3', 'Sarna');
-// var persona3 = CreateNewPatient('Juan','28', '19456345-3', 'Malaria');
-// var persona4 = CreateNewPatient('Daniela','50', '8928876-2', 'Esguince');
-// var persona5 = CreateNewPatient('Ignacio', '40', '10456345-5', 'Tetanos');
+persona1.agregarPaciente();
+persona2.agregarPaciente();
+persona3.agregarPaciente();
+persona4.agregarPaciente();
+persona5.agregarPaciente();
